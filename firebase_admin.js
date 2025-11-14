681ad2f584b422c84b170fd403f13c54c24d2dd5
happyboy1327-sketch <happyboy1327@gmail.com>
2025-11-14 14:58:02 +0900

    Update firebase_admin.js

--- firebase_admin.js
+++ firebase_admin.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// 서비스 계정 키를 환경 변수에서 로드
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+// 환경변수 FIREBASE_KEY_JSON 로부터 서비스 계정 JSON 파싱
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY_JSON);
 
 // Firebase 초기화 (중복 방지)
 if (!admin.apps.length) {
